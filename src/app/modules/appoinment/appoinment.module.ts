89d86a97e5b1f870fd3764528d4b50c1fb43306d
Haziel Ortiz <ya-yo22124@outlook.com>
2024-02-18 19:26:31 -0600

    Add: Modules

---
+++ src/app/modules/appoinment/appoinment.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AppoinmentRoutingModule } from './appoinment-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AppoinmentRoutingModule
+  ]
+})
+export class AppoinmentModule { }
